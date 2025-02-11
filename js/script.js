175ecb9f89790e661d5707d4b6d1a85da30e08ab
Khin Yin <khinyin1999@gmail.com>
2025-02-11 17:21:50 +0700

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// filter using javascript
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post").show("1000");
+    } else {
+      $(".post")
+        .not("." + value)
+        .hide("1000");
+      $(".post")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+});
+
